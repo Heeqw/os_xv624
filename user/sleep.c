900c92db1a050a0d12365922e5419f2afc56e712
HeYuzhou <yuzhouhe22@fudan.edu.cn>
2024-09-21 20:08:07 +0800

    [fix]: atoi non-negative return value

--- user/sleep.c
+++ user/sleep.c
@@ -9,13 +9,20 @@ int main(int argc, char *argv[]){
         exit(1);
     }
 
-    // 将输入参数转换为整数
-    int tm = atoi(argv[1]);
-    // 检查输入是否为非负数
-    if (tm < 0){
-        fprintf(2,"sleep: ticks must be non-negative\n");
-        exit(1);
+    // 检查输入是否为有效的正整数
+    int tm = 0;
+    for (int i = 0; argv[1][i] != '\0'; i++) {
+        if (i == 0 && argv[1][i] == '-') {
+            fprintf(2, "sleep: ticks must be non-negative\n");
+            exit(1);
+        }
+        if (argv[1][i] < '0' || argv[1][i] > '9') {
+            fprintf(2, "sleep: invalid input\n");
+            exit(1);
+        }
+        tm = tm * 10 + (argv[1][i] - '0');
     }
+
     // 调用系统的 sleep 函数
     sleep(tm);
     printf("(nothing happens for a little while)\n");
